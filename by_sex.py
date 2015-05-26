4f27ec6386a448e4f06f819bfb52ebebfab3cf80
a <a@ubuntu.ubuntu-domain>
2015-05-26 15:33:15 +0530

    modified by_sex.py

--- by_sex.py
+++ by_sex.py
@@ -1,27 +1,31 @@
 from __future__ import division
 import sys
 import csv
+import pandas as pd
+import numpy as np
+import re
+import math
 
 if __name__ == '__main__':
-	with open('data/train.csv', 'rb') as f:
-		reader = csv.reader(f)
-		s_f = 0
-		s_m = 0
-		d_f = 0
-		d_m = 0
-		cnt = 0
-		for row in reader:
-			if row[1] == '1' and row[4] == 'female':
-				s_f = s_f + 1
-			if row[1] == '1' and row[4] == 'male':
-				s_m = s_m + 1
-			if row[1] == '0' and row[4] == 'female':
-				d_f = d_f + 1
-			if row[1] == '0' and row[4] == 'male':
-				d_m = d_m + 1
-			cnt = cnt + 1
+	
+	s_f = 0
+	s_m = 0
+	d_f = 0
+	d_m = 0
 
-		print  'female survived = ', s_f/(s_f + d_f) * 100 
-		print  'male survived = ',s_m /(s_m + d_m) * 100
-		print  'female dead = ',d_f /  (s_f + d_f)*100
-		print  'male dead = ',d_m / (s_m + d_m)*100
+	from_csv = pd.read_csv('data/train.csv')
+
+	for i in range(0,len(from_csv)):
+		if from_csv['Sex'][i] == 'female' and from_csv['Survived'][i] == 1:
+			s_f = s_f + 1
+		if from_csv['Sex'][i] == 'male' and from_csv['Survived'][i] == 1:
+			s_m = s_m + 1
+		if from_csv['Sex'][i] == 'female' and from_csv['Survived'][i] == 0:
+			d_f = d_f + 1
+		if from_csv['Sex'][i] == 'male' and from_csv['Survived'][i] == 0:
+			d_m = d_m + 1
+
+	print  'female survived = ', s_f/(s_f + d_f) * 100 
+	print  'male survived = ',s_m /(s_m + d_m) * 100
+	print  'female dead = ',d_f /  (s_f + d_f)*100
+	print  'male dead = ',d_m / (s_m + d_m)*100
