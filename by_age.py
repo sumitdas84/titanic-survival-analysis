a8f806164f2fe31a5b248c07adb2572dadefe88f
a <a@ubuntu.ubuntu-domain>
2015-05-27 10:53:51 +0530

    by_age.py fills missing data for age and stores output into train_1.csv in data directory

--- by_age.py
+++ by_age.py
@@ -7,12 +7,13 @@ import re
 import math
 
 """
-this model find out name prefix like miss, master ... 
-we use name for assingning missing values of age
-
+this model find out name prefix like miss master
+we use name for assingning missing values of age 
 """
+
 if __name__ == '__main__':
 	
+	# finding out average of age according to "Mr.","Mrs.", etc. and store it in "age_grp_avg"
 	age_grp = {}
 	
 	from_csv = pd.read_csv('data/train.csv')
@@ -21,7 +22,7 @@ if __name__ == '__main__':
 		dumb = from_csv['Name'][i]
 		dumb = dumb.split(" ")
 		for prefix in dumb:
-			if "." in prefix and not math.isnan(float(from_csv['Age'][i])):
+			if "." in prefix and  not math.isnan(float(from_csv['Age'][i])):
 				if age_grp.has_key(prefix):
 					age_grp[prefix].append(float(from_csv['Age'][i]))
 					break
@@ -29,6 +30,19 @@ if __name__ == '__main__':
 					age_grp[prefix] = []
 					age_grp[prefix].append(float(from_csv['Age'][i]))
 					break
-	
+	# calculate average
+	age_grp_avg = {}
 	for key in age_grp:
-		print key, age_grp[key]
+		age_grp_avg[key] = reduce(lambda x, y: x + y, age_grp[key]) / len(age_grp[key])
+
+	# fill missing data
+	for i in range (0, len(from_csv)):
+		dumb = from_csv['Name'][i]
+		dumb = dumb.split(" ")
+		for prefix in dumb:
+			if "." in prefix and  math.isnan(float(from_csv['Age'][i])):
+				from_csv['Age'][i] = str(age_grp_avg[prefix])
+				break
+
+	# store output in train_1.csv
+	from_csv.to_csv('data/train_1.csv')
